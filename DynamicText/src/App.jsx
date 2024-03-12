9c727177d23043671e00eb2e57fbc4135a6e2249
matthewtran1 <matthewtran1215@gmail.com>
2024-03-11 21:39:31 -0700

    Removed underline off title

--- DynamicText/src/App.jsx
+++ DynamicText/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold ">
         Dynamic Text Animation Generator
       </h1>
 
