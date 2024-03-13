51b961e0d9b0312caed8adc1bd9a9e6528a16d8d
matthewtran1 <matthewtran1215@gmail.com>
2024-03-13 11:29:05 -0700

    testing panel

--- DynamicText/src/components/customPanel.jsx
+++ DynamicText/src/components/customPanel.jsx
@@ -12,8 +12,9 @@ const customPanel = () => {
 
         {/* Color */}
         <div className="">
-
-
+          <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white" type="button">Dropdown button 
+          <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6"></svg>
+          </button>
 
         </div>
 
