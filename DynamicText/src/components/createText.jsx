f382438e6553dbf06904e1758f8bee6c40eda78a
matthewtran1 <matthewtran1215@gmail.com>
2024-04-15 15:27:29 -0700

    format

--- DynamicText/src/components/createText.jsx
+++ DynamicText/src/components/createText.jsx
@@ -11,168 +11,169 @@ import anime from 'animejs/lib/anime.es.js';
 const CreateText = ({ selectedValues}) => {
 
 
+  const [isAnimating, setIsAnimating] = useState(false);
 
-    const animateText = () => {
-      
-      {/* Get the value in the input text box */}
-      const inputTextElement = document.getElementById("inputText").value;
+  const animateText = () => {
     
-      console.log(inputTextElement);
-      let classes = "";
-
-      {/* Add the selected value to the variable that holds the classes */}
-      if (selectedValues.fontsize) classes += ` ${selectedValues.fontsize}`;
-      if (selectedValues.fontStyle) classes += ` ${selectedValues.fontStyle}`;
-      if (selectedValues.color) classes += ` ${selectedValues.color}`;
-
-       {/* Print input text into the drawBox */}
-       {/* 
-      document.getElementById("animateText").innerText = inputTextElement;
-      document.getElementById("animateText").className = classes.trim();
-
-      // Reset rotation to 0 degrees to be able animate text again 
-      document.getElementById("animateText").style.transform = "rotate(0deg)";
-*/}
-      //Reset animation element
-      anime.remove('#animateText');
-
-      // Fade in Animation
-      if (selectedValues.animationStyle === "fadein") {
+    {/* Get the value in the input text box */}
+    const inputTextElement = document.getElementById("inputText").value;
   
-        anime({
-            targets: '#animateText',                    //Specifiy the element to be animated
-            opacity: [0, 1],                            //Opacity: defines the start and end of fade
-            duration: 2000,                             //Duration of animation in milliseconds
-            easing: 'easeInOutQuad',                    //Starts slowly, speeds up in the middle and then slows down again
-           
-            
-        });
-      }
+    console.log(inputTextElement);
+    let classes = "";
 
-      // Fade up animation
-      else if (selectedValues.animationStyle === "fadeup") {
+    {/* Add the selected value to the variable that holds the classes */}
+    if (selectedValues.fontsize) classes += ` ${selectedValues.fontsize}`;
+    if (selectedValues.fontStyle) classes += ` ${selectedValues.fontStyle}`;
+    if (selectedValues.color) classes += ` ${selectedValues.color}`;
 
-        anime({
-            targets: '#animateText',
-            opacity: [0, 1],
-            translateY: ['100%', '0%'],                 //direction of animation
-            easing: 'easeInOutQuad',
-            duration: 1500, 
-            
-    
-        });
-      }
+      {/* Print input text into the drawBox */}
       
-      // Fade down animation
-      else if (selectedValues.animationStyle === "fadedown") {
-    
-        anime({
-            targets: '#animateText',
-            opacity: [0, 1],
-            translateY: ['-100%', '0%'],            
-            easing: 'easeInOutQuad',
-            duration: 1500, 
+    document.getElementById("animateText").innerText = inputTextElement;
+    document.getElementById("animateText").className = classes.trim();
+
+    // Reset rotation to 0 degrees to be able animate text again 
+    document.getElementById("animateText").style.transform = "rotate(0deg)";
+
+    //Reset animation element
+    anime.remove('#animateText');
+
+    // Fade in Animation
+    if (selectedValues.animationStyle === "fadein") {
+
+      anime({
+          targets: '#animateText',                    //Specifiy the element to be animated
+          opacity: [0, 1],                            //Opacity: defines the start and end of fade
+          duration: 2000,                             //Duration of animation in milliseconds
+          easing: 'easeInOutQuad',                    //Starts slowly, speeds up in the middle and then slows down again
           
-       
-        });
-      }
-      
-      // Fade left animation
-      else if (selectedValues.animationStyle === "fadeleft") {
-   
-        anime({
-            targets: '#animateText',
-            opacity: [0, 1],
-            translateX: ['100%', '0%'],
-            easing: 'easeInOutQuad',
-            duration: 1500, 
-           
-           
-        });
-      }
-
-      // Fade right animation
-      else if (selectedValues.animationStyle === "faderight") {
-   
-        anime({
-            targets: '#animateText',
-            opacity: [0, 1],
-            translateX: ['-100%', '0%'],
-            easing: 'easeInOutQuad',
-            duration: 1500, 
-           
-       
-        });
-      }
-      // Rotate cw animation
-      else if (selectedValues.animationStyle === "rotateCW") {
-    
-        anime({
-            targets: '#animateText',
-            rotate: '360deg',
-            easing: 'linear',
-            duration: 2000, 
-        
-            
-        });
-      }
-
-      // Rotate ccw animation
-      else if (selectedValues.animationStyle === "rotateCCW") {
-      
-        anime({
-            targets: '#animateText',
-            rotate: '-360deg',
-            easing: 'linear',
-            duration: 2000, 
-            
-           
-        });
-      }
-
-      //flip animation
-      else if (selectedValues.animationStyle === "flip") {
-
-        anime({
-          targets: '#animateText',
-          rotateY: [0, 360], 
-          easing: 'easeInOutQuad',
-          duration: 2000,
-         
-
-        });
-      }
-
-      //return right animation
-      else if (selectedValues.animationStyle === "returnR")
-      {
-        
-        anime({
-          targets: '#animateText',
-          translateX: [0, 100], 
-          delay: 500,
-          direction: 'alternate',
-       
-   
-        });
-
-      }
-
-      //return left animation
-      else if (selectedValues.animationStyle === "returnL")
-      {
-    
-        anime({
-          targets: '#animateText',
-          translateX: [0, -100], 
-          delay: 500,
-          direction: 'alternate',
-         
-        });
-
-      }
-      console.log(selectedValues.fontsize, selectedValues.fontStyle, selectedValues.color, selectedValues.animationStyle)
+          
+      });
     }
+
+    // Fade up animation
+    else if (selectedValues.animationStyle === "fadeup") {
+
+      anime({
+          targets: '#animateText',
+          opacity: [0, 1],
+          translateY: ['100%', '0%'],                 //direction of animation
+          easing: 'easeInOutQuad',
+          duration: 1500, 
+          
+  
+      });
+    }
+    
+    // Fade down animation
+    else if (selectedValues.animationStyle === "fadedown") {
+  
+      anime({
+          targets: '#animateText',
+          opacity: [0, 1],
+          translateY: ['-100%', '0%'],            
+          easing: 'easeInOutQuad',
+          duration: 1500, 
+        
+      
+      });
+    }
+    
+    // Fade left animation
+    else if (selectedValues.animationStyle === "fadeleft") {
+  
+      anime({
+          targets: '#animateText',
+          opacity: [0, 1],
+          translateX: ['100%', '0%'],
+          easing: 'easeInOutQuad',
+          duration: 1500, 
+          
+          
+      });
+    }
+
+    // Fade right animation
+    else if (selectedValues.animationStyle === "faderight") {
+  
+      anime({
+          targets: '#animateText',
+          opacity: [0, 1],
+          translateX: ['-100%', '0%'],
+          easing: 'easeInOutQuad',
+          duration: 1500, 
+          
+      
+      });
+    }
+    // Rotate cw animation
+    else if (selectedValues.animationStyle === "rotateCW") {
+  
+      anime({
+          targets: '#animateText',
+          rotate: '360deg',
+          easing: 'linear',
+          duration: 2000, 
+      
+          
+      });
+    }
+
+    // Rotate ccw animation
+    else if (selectedValues.animationStyle === "rotateCCW") {
+    
+      anime({
+          targets: '#animateText',
+          rotate: '-360deg',
+          easing: 'linear',
+          duration: 2000, 
+          
+          
+      });
+    }
+
+    //flip animation
+    else if (selectedValues.animationStyle === "flip") {
+
+      anime({
+        targets: '#animateText',
+        rotateY: [0, 360], 
+        easing: 'easeInOutQuad',
+        duration: 2000,
+        
+
+      });
+    }
+
+    //return right animation
+    else if (selectedValues.animationStyle === "returnR")
+    {
+      
+      anime({
+        targets: '#animateText',
+        translateX: [0, 100], 
+        delay: 500,
+        direction: 'alternate',
+      
+  
+      });
+
+    }
+
+    //return left animation
+    else if (selectedValues.animationStyle === "returnL")
+    {
+  
+      anime({
+        targets: '#animateText',
+        translateX: [0, -100], 
+        delay: 500,
+        direction: 'alternate',
+        
+      });
+
+    }
+    console.log(selectedValues.fontsize, selectedValues.fontStyle, selectedValues.color, selectedValues.animationStyle)
+  }
 
 
     return (
@@ -184,7 +185,6 @@ const CreateText = ({ selectedValues}) => {
         className="text-white bg-slate-500 hover:bg-slate-600 font-medium rounded-lg text-md px-3 py-1.5">
 
           Animate
-
         </button>      
         
        
--- DynamicText/src/components/drawTextBox.jsx
+++ DynamicText/src/components/drawTextBox.jsx
@@ -11,14 +11,14 @@ const DrawTextBox = () => {
        
     
     return (
-      <canvas
+      <div
       id = "canvasId"
       className="flex items-center justify-center w-full h-96 rounded-lg border-2 border-slate-500 overflow-hidden">
        
   
-  
+        <p id="animateText" className=""></p>
       
-      </canvas>
+      </div>
     )
   }
 
